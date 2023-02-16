bf4f738ea5501b0ec3bb18ee82c321ccd54253be
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-02-16 14:13:06 +0100

    add external configuration

--- src/main/java/io/sewahshop/catalogservice/controllers/HomeController.java
+++ src/main/java/io/sewahshop/catalogservice/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package io.sewahshop.catalogservice.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+    @Value("${my.greeting}")
+    String greeting;
     @GetMapping("/")
     String getGreeting(){
-        return "Welcome to the Product Catalog Service";
+        return greeting;
     }
 }
