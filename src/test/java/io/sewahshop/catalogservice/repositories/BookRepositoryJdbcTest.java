97ceee2d6576be20fc2d807d732d9733f7445a6b
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-02-18 01:06:39 +0100

    implement test-containers

---
+++ src/test/java/io/sewahshop/catalogservice/repositories/BookRepositoryJdbcTest.java
@@ -0,0 +1,43 @@
+package io.sewahshop.catalogservice.repositories;
+
+import io.sewahshop.catalogservice.Config.PersistenceConfig;
+import io.sewahshop.catalogservice.entity.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJdbcTest
+@Import(PersistenceConfig.class)
+@AutoConfigureTestDatabase(
+        replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+class BookRepositoryJdbcTest {
+    @Autowired
+    private BookRepository  bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+
+    }
+}
