a0b5495a1a54e04932e7c284224cecf96f48a0c0
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-02-18 01:48:47 +0100

    update

--- src/test/java/io/sewahshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/io/sewahshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,9 +1,18 @@
 package io.sewahshop.catalogservice;
 
+import io.sewahshop.catalogservice.Config.PersistenceConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@Import(PersistenceConfig.class)
+@AutoConfigureTestDatabase(
+		replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
 	@Test
